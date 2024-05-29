8bcf85ca5a7a5cadbbbe21ec152f90341e092ae3
rukirukiruk <163348244+rukirukiruk@users.noreply.github.com>
2024-05-29 06:07:44 +0200

    implemented new feature

--- server/models.py
+++ server/models.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource, reqparse, fields, marshal_with
 import os
 from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables from .env.
 
 app = Flask(__name__)
+api = Api(app)
 # Assuming .env file has DATABASE_URL
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -32,5 +34,77 @@ class Task(db.Model):
     def __repr__(self):
         return f'<Task {self.description}>'
 
+# Flask-RESTful Resources
+project_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'description': fields.String,
+}
+
+task_fields = {
+    'id': fields.Integer,
+    'description': fields.String,
+    'priority': fields.Integer,
+    'deadline': fields.String,
+    'project_id': fields.Integer,
+}
+
+class ProjectListResource(Resource):
+    @marshal_with(project_fields)
+    def get(self):
+        projects = Project.query.all()
+        return projects
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', required=True, help="Name cannot be blank!")
+        parser.add_argument('description')
+        args = parser.parse_args()
+        
+        project = Project(name=args['name'], description=args.get('description'))
+        db.session.add(project)
+        db.session.commit()
+        return {'message': 'Project created', 'project': args}, 201
+
+class ProjectResource(Resource):
+    @marshal_with(project_fields)
+    def get(self, id):
+        project = Project.query.filter_by(id=id).first()
+        if not project:
+            return {'message': 'Project not found'}, 404
+        return project
+
+    def delete(self, id):
+        project = Project.query.filter_by(id=id).first()
+        if not project:
+            return {'message': 'Project not found'}, 404
+        db.session.delete(project)
+        db.session.commit()
+        return {'message': 'Project deleted'}, 200
+
+class TaskListResource(Resource):
+    @marshal_with(task_fields)
+    def get(self, project_id):
+        tasks = Task.query.filter_by(project_id=project_id).all()
+        return tasks
+
+    def post(self, project_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument('description', required=True, help="Description cannot be blank!")
+        parser.add_argument('priority', type=int)
+        parser.add_argument('deadline')
+        args = parser.parse_args()
+        
+        task = Task(description=args['description'], priority=args.get('priority'), deadline=args.get('deadline'), project_id=project_id)
+        db.session.add(task)
+        db.session.commit()
+        return {'message': 'Task created', 'task': args}, 201
+
+# Adding routes
+api.add_resource(ProjectListResource, '/projects')
+api.add_resource(ProjectResource, '/projects/<int:id>')
+api.add_resource(TaskListResource, '/projects/<int:project_id>/tasks')
+
 if __name__ == '__main__':
     db.create_all()
+    app.run(debug=True)
